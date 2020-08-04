e6307ed2539ecd0129b7126949c81ba38950276d
sbalan7 <sbalan7@outlook.com>
2020-08-04 13:44:27 +0530

    created files

---
+++ Challenge-Round-2/training.py
@@ -0,0 +1,9 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.svm import SVC
+import pandas as pd
+import numpy as np
+import pickle
+
+
